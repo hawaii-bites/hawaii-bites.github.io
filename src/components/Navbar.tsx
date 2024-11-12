4c66bdbeb84328e0ff9f9ef3bc4b0e1fc7da02c5
sethines <3chikara3@gmail.com>
2024-11-11 21:58:33 -1000

    update foods available to appear in nav and show webpage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar: React.FC = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Hawaii Bites</h1>
         <nav className="space-x-4">
-          <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/home" className="hover:underline">Home</Link>
           <Link href="/profile" className="hover:underline">Profile</Link>
-          <Link href="/todays-top-picks" className="hover:underline">Today's Top Picks</Link>
-          <Link href="/foods-available-right-now" className="hover:underline">Food's Available Right Now</Link>
+          <Link href="/todays-top-picks" className="hover:underline">Today&apos;s Top Picks</Link>
+          <Link href="/food-available-now" className="hover:underline">Food&apos;s Available Right Now</Link>
         </nav>
       </div>
     </header>
